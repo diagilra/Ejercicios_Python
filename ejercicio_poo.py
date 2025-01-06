dd3637b015d9b77d714468175c5e5e81e46be631
Diego Rave <diagilra@unal.edu.co>
2025-01-05 21:00:45 -0500

    Encapsulación

--- ejercicio_poo.py
+++ ejercicio_poo.py
@@ -1,39 +1,39 @@
 class Personaje:
     def __init__(self, nombre, fuerza, inteligencia, defensa, vida):
-        self.nombre = nombre
-        self.fuerza = fuerza
-        self.inteligencia = inteligencia
-        self.defensa = defensa
-        self.vida = vida
+        self.__nombre = nombre
+        self.__fuerza = fuerza
+        self.__inteligencia = inteligencia
+        self.__defensa = defensa
+        self.__vida = vida
 
     def atributos(self):
-        print(f"Nombre: {self.nombre}")
-        print(f"Fuerza: {self.fuerza}")
-        print(f"Inteligencia: {self.inteligencia}")
-        print(f"Defensa: {self.defensa}")
-        print(f"Vida: {self.vida}")
+        print(f"Nombre: {self.__nombre}")
+        print(f"Fuerza: {self.__fuerza}")
+        print(f"Inteligencia: {self.__inteligencia}")
+        print(f"Defensa: {self.__defensa}")
+        print(f"Vida: {self.__vida}")
     
     def subir_nivel(self, fuerza, inteligencia, defensa):
-        self.fuerza += fuerza
-        self.inteligencia += inteligencia
-        self.defensa += defensa
+        self.__fuerza += fuerza
+        self.__inteligencia += inteligencia
+        self.__defensa += defensa
 
     def esta_vivo(self):
-        return self.vida > 0
+        return self.__vida > 0
     
-    def morir(self):
-        self.vida = 0
-        print(f"El personaje {self.nombre } ha muerto")
+    def __morir(self):
+        self.__vida = 0
+        print(f"El personaje {self.__nombre } ha muerto")
 
     def dano(self, enemigo):
-        return  self.fuerza - enemigo.defensa
+        return  self.__fuerza - enemigo.__defensa
     
     def ataque(self, enemigo):
         dano = self.dano(enemigo)
-        enemigo.vida -= dano
-        print(f"{self.nombre} hizo un daño de {dano} a {enemigo.nombre}")
+        enemigo.__vida -= dano
+        print(f"{self.__nombre} hizo un daño de {dano} a {enemigo.__nombre}")
         if enemigo.esta_vivo():
-            print(f"La vida actual de {enemigo.nombre} es: {enemigo.vida}")
+            print(f"La vida actual de {enemigo.__nombre} es: {enemigo.__vida}")
         else:
             enemigo.morir()
 
