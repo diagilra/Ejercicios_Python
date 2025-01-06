8340b843583141c5bfca4a000937ce49aea5313a
Diego Rave <diagilra@unal.edu.co>
2025-01-06 00:06:50 -0500

    Correción de encapsulación y herencia

--- ejercicio_poo.py
+++ ejercicio_poo.py
@@ -1,46 +1,80 @@
 class Personaje:
     def __init__(self, nombre, fuerza, inteligencia, defensa, vida):
-        self.__nombre = nombre
-        self.__fuerza = fuerza
-        self.__inteligencia = inteligencia
-        self.__defensa = defensa
-        self.__vida = vida
+        self.nombre = nombre
+        self.fuerza = fuerza
+        self.inteligencia = inteligencia
+        self.defensa = defensa
+        self.vida = vida
 
     def atributos(self):
-        print(f"Nombre: {self.__nombre}")
-        print(f"Fuerza: {self.__fuerza}")
-        print(f"Inteligencia: {self.__inteligencia}")
-        print(f"Defensa: {self.__defensa}")
-        print(f"Vida: {self.__vida}")
+        print(f"Nombre: {self.nombre}")
+        print(f"Fuerza: {self.fuerza}")
+        print(f"Inteligencia: {self.inteligencia}")
+        print(f"Defensa: {self.defensa}")
+        print(f"Vida: {self.vida}")
     
     def subir_nivel(self, fuerza, inteligencia, defensa):
-        self.__fuerza += fuerza
-        self.__inteligencia += inteligencia
-        self.__defensa += defensa
+        self.fuerza += fuerza
+        self.inteligencia += inteligencia
+        self.defensa += defensa
 
     def esta_vivo(self):
-        return self.__vida > 0
+        return self.vida > 0
     
-    def __morir(self):
-        self.__vida = 0
-        print(f"El personaje {self.__nombre } ha muerto")
+    def morir(self):
+        self.vida = 0
+        print(f"El personaje {self.nombre } ha muerto")
 
     def dano(self, enemigo):
-        return  self.__fuerza - enemigo.__defensa
+        return  self.fuerza - enemigo.defensa
     
     def ataque(self, enemigo):
         dano = self.dano(enemigo)
-        enemigo.__vida -= dano
-        print(f"{self.__nombre} hizo un daño de {dano} a {enemigo.__nombre}")
+        enemigo.vida -= dano
+        print(f"{self.nombre} hizo un daño de {dano} a {enemigo.nombre}")
         if enemigo.esta_vivo():
-            print(f"La vida actual de {enemigo.__nombre} es: {enemigo.__vida}")
+            print(f"La vida actual de {enemigo.nombre} es: {enemigo.vida}")
         else:
             enemigo.morir()
 
-personaje1 = Personaje("Destructor", 350, 100, 180, 200)
-personaje1.atributos()
+class Guerrero(Personaje):
+    def __init__(self, nombre, fuerza, inteligencia, defensa, vida, arma ):
+        super().__init__(nombre, fuerza, inteligencia, defensa, vida)
+        self.arma = arma
 
-personaje2 = Personaje("Otro por ahí", 190, 102, 100, 210)
-personaje2.atributos()
+    def seleccionar_arma(self):
+        print("Seleccione un arma")
+        opcion = int(input("1. Espada valkiriana daño 8 \n 2. Espada matadragones daño 10 \n 3. Escopeta daño 15 \n 4. Lanzallamas daño 6 "))
+        if opcion == 1:
+            self.arma = 8
+        elif opcion == 2:
+            self.arma = 10
+        elif opcion == 3:
+            self.arma = 15
+        elif opcion == 4:
+            self.arma = 6
+        else:
+            print("Ingrese un valor co33rrecto")
 
-personaje1.ataque(personaje2)
+    def atributos(self):
+        super().atributos()
+        print(f"El daño del arma es: {self.arma} ")
+
+    def dano(self, enemigo):
+        return self.fuerza*self.arma-enemigo.defensa
+    
+
+
+
+guerrero1 = Guerrero("Laurent", 20, 80, 100, 150, 2)
+guerrero1.seleccionar_arma()
+guerrero1.atributos()
+
+
+# personaje1 = Personaje("Destructor", 350, 100, 180, 200)
+# personaje1.atributos()
+
+# personaje2 = Personaje("Otro por ahí", 190, 102, 100, 210)
+# personaje2.atributos()
+
+# personaje1.ataque(personaje2)
