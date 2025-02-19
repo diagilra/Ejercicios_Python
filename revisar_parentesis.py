558f5edd6a992e8e5a1ad9422e8a7c334c7d56a8
Diego Rave <diagilra@unal.edu.co>
2025-02-19 18:14:15 -0500

    Se agregó la función revisar_parentesis que utiliza deque

--- revisar_parentesis.py
+++ revisar_parentesis.py
@@ -1,3 +1,5 @@
+from collections import deque
+
 def revisar(s):
     stack=[]
     for elem in s:
@@ -12,7 +14,27 @@ def revisar(s):
         return False
     else:
         return True
-    
+
+# También puedo crear la función con un deque para que sea más rápido
+def revisar_parentesis(s):
+    deq=deque()
+    for elem in s:
+        if elem == "(":
+            deq.append(elem) 
+        elif elem == ")":
+            if len(deq)>0:
+                deq.pop()
+            else:
+                return False
+    if len(deq)!=0:
+        return False
+    else:
+        return True    
+
+
 ejemplo="(())((()))()"
 
+
+
 print(revisar(ejemplo))
+
